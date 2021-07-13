2b00d47910a55f5fff473e44b63d4e22e05abbe5
matheus-nicolay <nicolaymatheus222@gmail.com>
2021-07-13 00:26:12 -0300

    finished crud and added email sender function

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const appointmentService = require("./services/AppointmentService");
+const AppointmentService = require("./services/AppointmentService");
+const appointment = require("./models/Appointment");
 
 app.use(express.static("public"));
 
@@ -11,10 +13,11 @@ app.use(bodyParser.json());
 
 app.set('view engine','ejs');
 
-mongoose.connect("mongodb://localhost:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost:27017/agendamento",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useFindAndModify', false);
 
 app.get("/", (request, response) => {
-    response.send("Home!");
+    response.render("index.ejs");
 });
 
 app.get("/register", (request, response) => {
@@ -23,9 +26,53 @@ app.get("/register", (request, response) => {
 
 app.post("/create", async (request, response) => {
     const {name, email, cpf, description, date, time} = request.body; 
-    appointmentService.createAppointment(name, email, description, cpf, date, time); 
+
+    const status = await appointmentService.createAppointment(name, email, description, cpf, date, time); 
     
+    if(status){
+        response.redirect("/");
+    }else{
+        response.send("Ocorreu uma falha!");
+    }
 });
+
+app.get("/getcalendar", async (request, response) => {
+    const appointments = await AppointmentService.GetAppointments(false);
+    
+    response.json(appointments);
+});
+
+app.get("/event/:id", async (request, response) => {
+    const appointment = await AppointmentService.getAppointmentById(request.params.id);
+    response.render("event.ejs", {appointment: appointment});
+});
+
+app.post("/finish", async (request, response) => {
+    const id = request.body.id;
+    const finishAppointment = await AppointmentService.FinishAppointment(id);
+    if(finishAppointment){
+        response.redirect("/");
+    }
+});
+
+app.get("/appointments", async (request, response) =>{
+    const appos = await AppointmentService.GetAppointments(true);
+    response.render("list.ejs", {appointments: appos});
+});
+
+app.get("/searchresults/", async (request, response) =>{
+    var query = request.query.search;
+    const appos = await AppointmentService.Search(query);
+    response.render("list.ejs", {appointments: appos});
+});
+
+
+var pollTime = 5000;
+
+setInterval(async () =>{
+    console.log("rodando poll")
+    await AppointmentService.SendNotification();
+}, pollTime);
 
 app.listen(80, () => {
     console.log("Server is running");
--- services/AppointmentService.js
+++ services/AppointmentService.js
@@ -1,18 +1,22 @@
 const appointment = require("../models/Appointment");
 const mongoose = require("mongoose");
+const AppointmentFactory = require("../factories/AppointmentFactory");
+const mailer =  require("nodemailer");
 
-const Appointment = mongoose.model("Appointment", appointment);
+const Appo = mongoose.model("Appointment", appointment);
 
 class Appointment{
 
     async createAppointment(name, email, description, cpf, date, time){
-        const newAppointment = new Appointment({
+        const newAppointment = new Appo({
             name, 
             email, 
             description,
             cpf, 
             date,
-            time
+            time,
+            finished: false,
+            notified: false,
         });
         try{    
             await newAppointment.save();
@@ -21,6 +25,99 @@ class Appointment{
             console.log(error);
             return false;
         } 
+    }
+
+    async GetAppointments(showFinished){
+        if(showFinished){
+            return await Appo.find();
+        }else{
+            const appos = await Appo.find({'finished': false});
+
+            const appointments = [];
+
+            
+            appos.forEach(appointment =>{
+                if(appointment.date != undefined){
+                    appointments.push(AppointmentFactory.Build(appointment));
+                }
+            });
+
+            return appointments;
+        }
+    }
+
+    async getAppointmentById(id){
+        if(id){
+            try{
+                const appo = await Appo.findOne({'_id': id});
+                return appo;
+            }catch(error){
+                console.log(error);
+            }
+        }else{
+            console.log("ID inválido");
+            return false;
+        }
+    }
+
+    async FinishAppointment(id){
+        try{
+            await Appo.findByIdAndUpdate(id,{finished: true});
+            return true;
+        }catch(error){
+            console.log(error);
+            return false;
+        }
+    }
+
+    async Search(query){
+        try{
+            const appos = await Appo.find().or([{name: query}, {cpf: query}]);
+            return appos;
+        }catch(error){
+            console.log(error);
+            return [];
+        }
+      
+    }
+
+    async SendNotification(){
+        var appos = await this.GetAppointments(false);
+        
+        var transporter = mailer.createTransport({
+            host: "smtp.mailtrap.io" ,
+            port: 25,
+            auth: {
+                user: "26bd3c8988e231",
+                pass: "4911e889433a2b"
+            }
+         });
+
+        appos.forEach(async app => {
+            
+            var date = app.start.getTime(); 
+            var hour = 1000 * 60 * 60;
+            var gap = date-Date.now();
+
+            if(gap <= hour){
+                
+                if(app.notified){
+                    
+                    transporter.sendMail({
+                        from: "Victor Lima <victor@guia.com.br>",
+                        to: app.email,
+                        subject: "Sua consulta vai acontecer em breve!",
+                        text: "Conteúdo qualquer!!!!! Sua consulta vai acontecer em 1h"
+                    }).then( () => {
+                        
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                    await Appo.findByIdAndUpdate(app.id,{notified: true});
+                }
+            }
+
+        })
     }
 }
 
